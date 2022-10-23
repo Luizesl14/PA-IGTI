30b37dbacd81b0f5a2d823d9dbd5caf83db45453
Luizesl14 <luiz.develop@gmail.com>
2022-10-23 01:09:51 -0300

    Class TenantAwareTaskDecorator

---
+++ src/main/java/igti/pa/multitenancy/application/async/TenantAwareTaskDecorator.java
@@ -0,0 +1,20 @@
+package igti.pa.multitenancy.application.async;
+
+import igti.pa.multitenancy.application.core.ThreadLocalStorage;
+import org.springframework.core.task.TaskDecorator;
+
+public class TenantAwareTaskDecorator implements TaskDecorator {
+
+    @Override
+    public Runnable decorate(Runnable runnable) {
+        String tenantName = ThreadLocalStorage.getTenantName();
+        return () -> {
+            try {
+                ThreadLocalStorage.setTenantName(tenantName);
+                runnable.run();
+            } finally {
+                ThreadLocalStorage.setTenantName(null);
+            }
+        };
+    }
+}
